06f883f924737901f5d7f6f2fd85a0edf2f30d6a
Vijay <vijay07appu@gmail.com>
2026-02-05 21:09:23 +0530

    CommandLine operation feature added

--- src/main/java/com/springBootProject/githubActivity/runner/GitHubRunner.java
+++ src/main/java/com/springBootProject/githubActivity/runner/GitHubRunner.java
@@ -1,14 +1,46 @@
 package com.springBootProject.githubActivity.runner;
 
 
+import com.springBootProject.githubActivity.service.GithubActivitySevice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Scanner;
 
 @Component
 public class GitHubRunner implements CommandLineRunner {
 
+    @Autowired
+    GithubActivitySevice githubActivitySevice;
+
+    private ObjectMapper mapper=new ObjectMapper();
+
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Welcome to GitActivity");
+        System.out.println();
+        System.out.println("Please enter you userName");
+        Scanner sc=new Scanner(System.in);
+        String userName=sc.nextLine();
+        Mono<String> mono=githubActivitySevice.getApi(userName);
+        mono.subscribe(data->{
+            try{
+
+                Object objectData=mapper.readValue(data,Object.class);
+                String stringData=mapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectData);
+                System.out.println(stringData);
+
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+
+        });
 
     }
 }
