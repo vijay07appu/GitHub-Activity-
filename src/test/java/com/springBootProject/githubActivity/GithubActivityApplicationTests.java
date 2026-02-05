3a5ce68bbe7b6eb590861f3f5dc0da48bac44858
Vijay <vijay07appu@gmail.com>
2026-02-05 09:25:57 +0530

    Github connected to this project

---
+++ src/test/java/com/springBootProject/githubActivity/GithubActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootProject.githubActivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
